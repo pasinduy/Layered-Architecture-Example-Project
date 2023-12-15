3b0cd81cd225c5d176adaf11ca131590e06913fa
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2023-12-15 10:12:38 +0530

    first commit

---
+++ src/main/java/com/example/layeredarchitecture/controller/SearchOrderFormController.java
@@ -0,0 +1,46 @@
+package com.example.layeredarchitecture.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class SearchOrderFormController {
+    public AnchorPane root;
+    public JFXButton btnAddNewCustomer;
+    public TextField txtCustomerAddress;
+    public TextField txtCustomerId;
+    public TextField txtCustomerName;
+    public JFXButton btnDelete;
+    public JFXButton btnSave;
+    public TableView tblCustomers;
+
+    public void navigateToHome(MouseEvent mouseEvent) throws IOException {
+        URL resource = this.getClass().getResource("/com/example/layeredarchitecture/main-form.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        Platform.runLater(() -> primaryStage.sizeToScene());
+    }
+
+    public void btnAddNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -0,0 +1,36 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.CustomerDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl {
+    public ArrayList<CustomerDTO> getAllCustomer() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+
+        ArrayList<CustomerDTO> getAllCustomer = new ArrayList<>();
+
+        while (rst.next()){
+            CustomerDTO customerDTO = new CustomerDTO(
+                    rst.getString("id"),
+                    rst.getString("name"),
+                    rst.getString("address")
+            );
+            getAllCustomer().add(customerDTO);
+        }
+        return getAllCustomer;
+    }
+
+    public void addCustomer() throws SQLException, ClassNotFoundException {
+        /*Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1, id);
+        pstm.setString(2, name);
+        pstm.setString(3, address);
+        pstm.executeUpdate();*/
+    }
+}
