106f89f87c13afe9bde9234f655d4afb867f19ff
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2023-12-16 22:30:10 +0530

    Trying to Fix the Search Order Form Controller

--- src/main/java/com/example/layeredarchitecture/controller/SearchOrderFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/SearchOrderFormController.java
@@ -1,13 +1,9 @@
 package com.example.layeredarchitecture.controller;
 
-import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -17,13 +13,6 @@ import java.net.URL;
 
 public class SearchOrderFormController {
     public AnchorPane root;
-    public JFXButton btnAddNewCustomer;
-    public TextField txtCustomerAddress;
-    public TextField txtCustomerId;
-    public TextField txtCustomerName;
-    public JFXButton btnDelete;
-    public JFXButton btnSave;
-    public TableView tblCustomers;
 
     public void navigateToHome(MouseEvent mouseEvent) throws IOException {
         URL resource = this.getClass().getResource("/com/example/layeredarchitecture/main-form.fxml");
@@ -33,14 +22,5 @@ public class SearchOrderFormController {
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
         Platform.runLater(() -> primaryStage.sizeToScene());
-    }
-
-    public void btnAddNew_OnAction(ActionEvent actionEvent) {
-    }
-
-    public void btnSave_OnAction(ActionEvent actionEvent) {
-    }
-
-    public void btnDelete_OnAction(ActionEvent actionEvent) {
     }
 }
