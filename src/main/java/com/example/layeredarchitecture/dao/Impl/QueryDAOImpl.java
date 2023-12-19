90365369c5ac007196d7d8bbf2fc7bdebb141684
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2023-12-19 15:59:07 +0530

    QueryDAO Files

--- src/main/java/com/example/layeredarchitecture/dao/Impl/QueryDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/Impl/QueryDAOImpl.java
@@ -1,5 +1,11 @@
 package com.example.layeredarchitecture.dao.Impl;
 
-public class QueryDAOImpl {
+import com.example.layeredarchitecture.dao.QueryDAO;
 
+public class QueryDAOImpl implements QueryDAO {
+
+    @Override
+    public void customerOrderDetails() {
+        //Join Query
+    }
 }
--- src/main/java/com/example/layeredarchitecture/dao/QueryDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/QueryDAO.java
@@ -1,5 +1,5 @@
 package com.example.layeredarchitecture.dao;
 
 public interface QueryDAO {
-
+    void customerOrderDetails();
 }
