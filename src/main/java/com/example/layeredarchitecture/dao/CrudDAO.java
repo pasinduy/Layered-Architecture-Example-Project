5d4948040db66543706d1a77ccd97d2f16fd6f8f
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2023-12-22 12:16:00 +0530

    DAOFactory and SuperDAO implementation

--- src/main/java/com/example/layeredarchitecture/dao/CrudDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/CrudDAO.java
@@ -3,7 +3,7 @@ package com.example.layeredarchitecture.dao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface CrudDAO<T>{
+public interface CrudDAO<T> extends SuperDAO{
     ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
     boolean save(T t) throws SQLException, ClassNotFoundException;
     boolean update(T t) throws SQLException, ClassNotFoundException;
